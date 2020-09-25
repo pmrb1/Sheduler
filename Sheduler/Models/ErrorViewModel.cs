4838426c2bbc8289585c658d417ec4b3ef9fab72
Roman Dmytrenko <dmytrenko22021992@gmail.com>
2020-09-25 18:19:34 +0300

    Добавьте файлы проекта.

---
+++ Sheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
